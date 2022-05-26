533e7da754b577e5c209f1bd523c291335222c9a
Juliano Nascimento <juliano120@gmail.com>
2022-05-26 03:19:26 -0300

    sitemap crawler

--- src/casas_crawler/casas_crawler/settings.py
+++ src/casas_crawler/casas_crawler/settings.py
@@ -21,14 +21,14 @@ ROBOTSTXT_OBEY = False
 USER_AGENT = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 32
+CONCURRENT_REQUESTS_PER_DOMAIN = 4
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
